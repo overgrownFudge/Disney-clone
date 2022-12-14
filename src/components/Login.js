b03f7369deb44cee972f885f841551ffced43667
overgrownFudge <overgrownfudge@gmail.com>
2022-12-14 15:26:55 -0500

    created login component with styled components

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Container>
+      <Content>Content</Content>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+`;
+
+const Content = styled.div``;
+
+export default Login;
