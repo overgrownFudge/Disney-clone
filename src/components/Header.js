68a4cad17c77840b54bb4f733ffc4a2d7dbcff2e
overgrownFudge <overgrownfudge@gmail.com>
2022-12-15 17:25:58 -0500

    added login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ export const Header = (props) => {
           <span>Series</span>
         </a>
       </NavMenu>
+      <Login>Login</Login>
     </Nav>
   );
 };
@@ -128,4 +129,19 @@ const NavMenu = styled.div`
   }
 `;
 
+const Login = styled.a`
+  background-color: rbga(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    cursor: pointer;
+  }
+`;
 export default Header;
