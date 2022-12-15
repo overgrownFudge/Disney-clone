5beaa9ac79b3c34843aedd688f319250e72c921e
overgrownFudge <overgrownfudge@gmail.com>
2022-12-15 17:24:38 -0500

    completed nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Header = () => {
+export const Header = (props) => {
   return (
     <Nav>
       <Logo>
         <img src='/assets/logo.svg' alt='Disney+' />
       </Logo>
-      <NavMenu>Menu</NavMenu>
+      <NavMenu>
+        <a href='/home'>
+          <img src='/assets/home-icon.svg' alt='Home' />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src='/assets/search-icon.svg' alt='Search' />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src='/assets/watchlist-icon.svg' alt='Watchlist' />
+          <span>Watchlist</span>
+        </a>
+        <a>
+          <img src='/assets/original-icon.svg' alt='Original' />
+          <span>Original</span>
+        </a>
+        <a>
+          <img src='/assets/movie-icon.svg' alt='Movies' />
+          <span>Movies</span>
+        </a>
+        <a>
+          <img src='/assets/series-icon.svg' alt='Series' />
+          <span>Series</span>
+        </a>
+      </NavMenu>
     </Nav>
   );
 };
@@ -51,6 +76,52 @@ const NavMenu = styled.div`
   position: relative;
   margin-right: auto;
   margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 2px 0;
+      white-space: nowrap;
+      position: relative;
+
+      &::before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        right: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
 
   @media (max-width: 768px) {
     display: none;
