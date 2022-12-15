3f6fd3d9cd4b406f04e14ca73268de8c6abdc683
overgrownFudge <overgrownfudge@gmail.com>
2022-12-15 15:09:15 -0500

    imported header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Routes>
         <Route path='/' element={<Login />} />
       </Routes>
