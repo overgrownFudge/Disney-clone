3c459a13c48fdb67beeef0484badeeee6e8d6b84
overgrownFudge <overgrownfudge@gmail.com>
2022-12-15 15:27:43 -0500

    completed logo section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Nav>
+      <Logo>
+        <img src='/assets/logo.svg' alt='Disney+' />
+      </Logo>
+      <NavMenu>Menu</NavMenu>
+    </Nav>
+  );
+};
 
-export default Header
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 8;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export default Header;
