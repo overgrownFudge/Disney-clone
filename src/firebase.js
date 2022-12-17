d878c52a121dae628913ebf01d6357e05aa980b4
overgrownFudge <100824616+overgrownFudge@users.noreply.github.com>
2022-12-16 20:14:48 -0500

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRxsfRcxqtfARirjsjAnac-Rl-ibM7ir0",
+
+  authDomain: "disneyplus-clone-6cf55.firebaseapp.com",
+
+  projectId: "disneyplus-clone-6cf55",
+
+  storageBucket: "disneyplus-clone-6cf55.appspot.com",
+
+  messagingSenderId: "761205832204",
+
+  appId: "1:761205832204:web:047a4acfad5ccb10b44c29",
+
+  measurementId: "G-H6M8XQP3V0",
+};
+
+const firebaseApp = firebase.initialilizeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
