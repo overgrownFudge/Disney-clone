aa1cd6e6b110c86ccec2ee7746c1d7b0dc792c4e
overgrownFudge <overgrownfudge@gmail.com>
2022-12-17 16:19:29 -0500

    fixed firebase imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore';
+import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCRxsfRcxqtfARirjsjAnac-Rl-ibM7ir0",
@@ -18,11 +19,11 @@ const firebaseConfig = {
   measurementId: "G-H6M8XQP3V0",
 };
 
-const firebaseApp = firebase.initialilizeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
+// const storage = firebase.storage();
 
-export { auth, provider, storage };
+export { auth, provider /*, storage */};
 export default db;
