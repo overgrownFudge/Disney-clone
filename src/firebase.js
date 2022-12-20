dfd1d364a153d2cc614b64c4ce39a33f3449e09e
overgrownFudge <overgrownfudge@gmail.com>
2022-12-19 20:30:09 -0500

    uncommented storage function

--- src/firebase.js
+++ src/firebase.js
@@ -24,7 +24,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-// const storage = firebase.storage();
+const storage = firebase.storage();
 
 export { auth, provider /*, storage */};
 export default db;
