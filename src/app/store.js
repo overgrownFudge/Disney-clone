6f961914441a39321f27ab16aae6894fc26cbdf9
overgrownFudge <overgrownfudge@gmail.com>
2022-12-25 17:38:30 -0500

    setup reducer for configureStore

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from '..userSlice'
 
 export default configureStore({
-    reducer:{},
+    reducer:{
+        user: userReducer
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
     })
