2a814a587c57cf3e6ada1643cf6bbc9819fe27b5
overgrownFudge <overgrownfudge@gmail.com>
2022-12-24 19:44:07 -0500

    setup configureStore

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducer:{},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+})
